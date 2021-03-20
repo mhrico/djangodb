00eb4b264761cbc2c7a0142a35c0114c6dcc316e
Mehedi Hassan Rico <mhrico2@gmail.com>
2021-03-20 15:45:44 +0600

    Authentication implementation complete

--- djangodb/seminar/views.py
+++ djangodb/seminar/views.py
@@ -4,11 +4,19 @@ from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import Group
+from django.contrib.auth.decorators import login_required, user_passes_test
 
 from .models import Student
 from .forms import CreateUserForm, StudentDataForm 
 
 # Create your views here.
+
+def is_student(user):
+    return user.groups.filter(name='STUDENT').exists()
+
+def is_librarian(user):
+    return user.groups.filter(name='LIBRARIAN').exists()
+
 def home(request):
     return render(request, 'home.html', {})
 
@@ -51,12 +59,18 @@ def studentlogin(request):
 
     return render(request, 'studentlogin.html', {})
 
+@login_required(login_url='studentlogin')
+@user_passes_test(is_student)
 def studentlanding(request):
     return render(request, 'studentlanding.html', {})
 
+@login_required(login_url='studentlogin')
+@user_passes_test(is_student)
 def requestbooks(request):
     return render(request, 'requestbooks.html', {})
 
+@login_required(login_url='studentlogin')
+@user_passes_test(is_student)
 def viewissued(request):
     return render(request, 'viewissued.html', {})
 
@@ -92,12 +106,18 @@ def librarianlogin(request):
 
     return render(request, 'librarianlogin.html', {})
 
+@login_required(login_url='librarianlogin')
+@user_passes_test(is_librarian)
 def librarianlanding(request):
     return render(request, 'librarianlanding.html', {})
 
+@login_required(login_url='librarianlogin')
+@user_passes_test(is_librarian)
 def issuebooks(request):
     return render(request, 'issuebooks.html', {})
 
+@login_required(login_url='librarianlogin')
+@user_passes_test(is_librarian)
 def updateissues(request):
     return render(request, 'updateissues.html', {})
 
